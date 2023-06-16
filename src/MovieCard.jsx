1d59f245661bf75060e3ca69203df720e4541808
niloocampos <nvancity@gmail.com>
2023-06-15 21:18:20 -0700

    new files added

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
 
-const MovieCard = ({ movie }) => {
-    return (
-        <div className=''>
-            <div className='movie mb-20'>
-                <div className='flex justify-center'>
-                    <p className='text-2xl'>{movie.Year}</p>
-                </div>
-                <div className='flex justify-center'>
-                    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
-                </div>
-
-                <div>
-                    <span className='text-xl'>{movie.Type}</span>
-                    <h3 className='text-2xl font-medium text-white'>{movie.Title}</h3>
-                </div>
-
-            </div>
-
+function MovieCard({ type, title, year, poster }) {
+  return (
+    <div className="">
+      <div className="movie mb-20">
+        <div className="flex justify-center">
+          <p className="text-2xl">{year}</p>
         </div>
-    )
+        <div className="flex justify-center">
+          <img src={poster !== 'N/A' ? poster : 'https://via.placeholder.com/400'} alt={title} />
+        </div>
+
+        <div>
+          <span className="text-xl">{type}</span>
+          <h3 className="text-2xl font-medium text-white">{title}</h3>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
+
+MovieCard.defaultProps = {
+  type: '',
+  year: '',
+  poster: '',
+  title: '',
+};
+
+MovieCard.propTypes = {
+  type: PropTypes.string,
+  year: PropTypes.string,
+  poster: PropTypes.string,
+  title: PropTypes.string,
+};
 
 export default MovieCard;
